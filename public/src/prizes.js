36a3a9f126e069ced91dde4a5cb940f442794003
pajmd <philippejmd@gmail.com>
2020-03-06 10:04:42 +0100

    Moved prizes to images

--- public/src/prizes.js
+++ public/src/prizes.js
@@ -5,10 +5,35 @@ import {
 } from './settings.js'
 
 const prizes = function(context) {
-    const colors = ['yellow', 'DarkTurquoise', 'brown'];
+    const colors = ['yellow', 'DarkTurquoise', 'blue'];
+    const images = [
+        document.getElementById('sherries'),
+        document.getElementById('duck'),
+        document.getElementById('boat'),
+    ];
+    // This would need a promise to work see sleep and waiting below
+    const image = function (src) {
+        const img = document.createElement("img");
+        img.src = src;
+        img.setAttribute("preload", "auto");
+        img.setAttribute("controls", "none");
+        img.style.display = "none";
+        document.body.appendChild(img);
+        return img;
+    };
+    const sleep = function (ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+      
+    async function waiting(ms) {
+        console.log('Taking a break...', ms);
+        await sleep(ms);
+        console.log(ms, ' miliseconds later...');
+    }
 
     const prizesSet = {
         location: [],
+        sherries: image('src/images/2Sherries.png'),
         isHorizontal: function(line) {
             return (line.end.x - line.start.x) ? true : false;
         },
@@ -18,7 +43,8 @@ const prizes = function(context) {
             return mid;
         },
         sprinkle: function(parcours) {
-            for (let i=0; i < parcours.layout.length; i+=2) {
+            // TODO put back all prizes
+            for (let i=0; i < 10 /*parcours.layout.length*/; i+=2) {
                 if (!parcours.layout[i].noBeastStart) {
                     if (this.isHorizontal(parcours.layout[i])) {
                         this.location.push(
@@ -30,7 +56,8 @@ const prizes = function(context) {
                                 ),
                                 color: colors[i % colors.length],
                                 line: parcours.layout[i],
-                                active: true
+                                active: true,
+                                image: images[i % images.length]
                             }
                         );
                     }
@@ -44,7 +71,8 @@ const prizes = function(context) {
                                 ),
                                 color: colors[i % colors.length],
                                 line: parcours.layout[i],
-                                active: true
+                                active: true,
+                                image: images[i % images.length]
                             }
                         );
                     }
@@ -56,9 +84,11 @@ const prizes = function(context) {
             this.location.forEach(prize => {
                 prize.active = true;
                 context.beginPath();
-                context.fillStyle = prize.color;
-                context.arc(prize.point.getX(), prize.point.getY(), PAC_WIDTH / 2, 0, 2 * Math.PI);
-                context.fill();
+                // context.fillStyle = prize.color;
+                // context.arc(prize.point.getX(), prize.point.getY(), PAC_WIDTH / 2, 0, 2 * Math.PI);
+                // context.fill();
+                console.log('Draw cherries');
+                context.drawImage(prize.image, prize.point.getX() -10, prize.point.getY()-10);
                 context.closePath();        
             });
         },
@@ -69,24 +99,7 @@ const prizes = function(context) {
                     this.location[i].active) {
 
                     if (!beast) {
-                        // const that = this;
-                        // return function() {
-                        //     context.beginPath();
-                        //     context.fillStyle = that.location[i].color;
-                        //     context.arc(
-                        //         that.location[i].point.getX(), 
-                        //         that.location[i].point.getY(), 
-                        //         PAC_WIDTH / 2, 0, 2 * Math.PI
-                        //     );
-                        //     context.fill();
-                        //     context.closePath();        
-                        // };
-                    //     return i;
-                    // }
-                    // else {
                         this.location[i].active = false;
-                        // this.location.splice(i, 1);
-                        // return this.location.length;
                     }
                     return i;
                 }
@@ -94,13 +107,16 @@ const prizes = function(context) {
         },
         redrawPrize: function(prizeIndex){
             context.beginPath();
-            context.fillStyle = this.location[prizeIndex].color;
-            context.arc(
-                this.location[prizeIndex].point.getX(), 
-                this.location[prizeIndex].point.getY(), 
-                PAC_WIDTH / 2, 0, 2 * Math.PI
-            );
-            context.fill();
+            // context.fillStyle = this.location[prizeIndex].color;
+            // context.arc(
+            //     this.location[prizeIndex].point.getX(), 
+            //     this.location[prizeIndex].point.getY(), 
+            //     PAC_WIDTH / 2, 0, 2 * Math.PI
+            // );
+            // context.fill();
+            context.drawImage(this.location[prizeIndex].image, 
+                this.location[prizeIndex].point.getX() -10, 
+                this.location[prizeIndex].point.getY()-10);
             context.closePath();        
         },
         areAllLocationsInactive: function() {
